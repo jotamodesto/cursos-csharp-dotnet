cb5c8af170402f446afdda2ff2657efc45f0af25
Johnatan Soares Modesto <johnatan.modesto@gmail.com>
2018-12-03 17:01:09 -0200

    Exercício Cap 9

---
+++ DesignPatterns2/DesignPatterns2/Cap9/Servico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns2.Cap9
+{
+    class Servico
+    {
+        private static Servico servico;
+        public static Servico Instancia
+        {
+            get
+            {
+                if (servico == null)
+                    servico = new Servico();
+
+                return servico;
+            }
+        }
+
+        private Servico()
+        {
+
+        }
+    }
+}
